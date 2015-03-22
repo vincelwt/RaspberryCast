5faabe01e1e889a25d2a34f72fd44877cc40ba0c
vince <vincent.loewert@gmail.com>
2015-03-22 13:52:28 +0100

    Both sound by default

--- config.py
+++ config.py
@@ -3,8 +3,5 @@
 # Set to True if you have a slow Internet connection, it will stream videos in 360p = False
 low_mode = True
 
-# Time before it start streaming the video, for preloading. Useful if you have a slow internet connection. Default is 0.
-time_preloading = 10
-
-# Audio output : hdmi (default) or local (using the jack)
-audio_output = "hdmi"
+# Audio output : hdmi or local (using the jack) or both (jack & hdmi) (default)
+audio_output = "both"
