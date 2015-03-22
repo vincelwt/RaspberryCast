3379ccdc8e669108474dc86286bfc41ad6156b1b
vince <vincent.loewert@gmail.com>
2015-03-22 13:49:34 +0100

    logging system

--- process.py
+++ process.py
@@ -3,23 +3,29 @@
 import subprocess, os, signal, sys, re
 from time import *
 from config import *
+import logging
 
 def launchvideo(url, sub):
-
-	#Wake up the screen
-	os.system("echo -ne '\033[9;0]' >/dev/tty1")
+	try :
+		os.system("echo -ne '\033[9;0]' >/dev/tty1")
+	except :
+		logging.error('Unable to wake up screen. Giving up.')
 
 	if is_running() == True : 
 		os.system("killall omxplayer.bin")
 
 	os.system("cat images/url.asc | wall")
+	logging.info('Trying to retrieve video URL...')
+	
 
 	if (url[-4:] in (".avi", ".mkv", ".mp4", ".mp3")) or (sub == True) :	
 		print "No need to Youtube-dl."	
+		logging.info('Direct file, no need to youtube-dl.')
 		out = url
 		
 	else :
 		if low_mode == True:
+			logging.info('Fast mode enabled (360p).')
 			if url[0:14] in ("https://youtu.", "http://youtu.b", "https://www.yo", "http://www.you", "http://youtub", "http://youtube") :
 				print "Youtube, setting to 360p"
 				command = "youtube-dl -f 18 -g "
@@ -29,26 +35,38 @@ def launchvideo(url, sub):
 			else :
 				command = "youtube-dl -g "
 		else:
+			logging.info('Normal mode, trying to extract full url.')
 			print "Trying to extract full url..."
 			command = "youtube-dl -g "
 
 	
+		try :
+			proc = subprocess.Popen(command+url, stdout=subprocess.PIPE, shell=True)
 
-		proc = subprocess.Popen(command+url, stdout=subprocess.PIPE, shell=True)
-
-		(out, err) = proc.communicate()
-
-		print "Full video URL is : ", out
+			(out, err) = proc.communicate()
+		except :
+			logging.error('Error with youtube-dl. Giving up.')
+		
 		out = out.rstrip()
 
+	logging.info("Full video URL is : " + out)	
+
 	os.system("cat images/omx.asc | wall")
+	logging.info("Trying to start OMX")
 	
 	if sub == True :
+		logging.info('Starting OMX with subtitles.')
 		omx = "omxplayer -b -o "+audio_output+" '"+out+"' --subtitles subtitle.srt < /tmp/cmd"
+		
 	else :
+		logging.info('Starting OMX without subtitles.')
 		omx = "omxplayer -b -o "+audio_output+" '"+out+"' < /tmp/cmd"
+			
 
-	os.system(omx+" &")
+	try :
+		os.system(omx+" &")
+	except :
+		logging.error('Unable to start OMX. Giving up.')
 
 	os.system("echo . > /tmp/cmd")
 
--- server.py
+++ server.py
@@ -3,14 +3,17 @@
 from bottle import *
 from process import *
 import os
+import logging
+
+logging.basicConfig(filename='RaspberryCast.log',level=logging.DEBUG)
 
 #Trying to create the FIFO is it is the 1st time
 os.system("mkfifo /tmp/cmd")
 
 os.system("cat images/cast.asc | wall")
+logging.info('RaspberryCast started.')
 
 app = Bottle()
-
 
 SimpleTemplate.defaults["get_url"] = app.get_url
 
@@ -26,50 +29,86 @@ def index():
 @app.route('/stream')	
 def stream(): 
 	url = request.query['url']	
-	print "casting now : " + url
-	launchvideo(url, False)
+	logging.info('Casting URL: '+url)
+	try :
+		launchvideo(url, False)
+	except :
+		logging.error('Error in launchvideo function.')
+		os.system("cat images/error.asc | wall")
 
 @app.route('/queue')
 def queue():
         url = request.query['url']
+	
 	if is_running() == True :
-		print "adding to queue : " + url
+		logging.info('Adding to queue: '+url)
+
 		#Writing url to file
 		with open('video.queue', 'a') as f:
 			f.write(url+'\n')
 	else :
-		print "casting now : " + url
-		launchvideo(url, False)
+		logging.info('Casting URL (no video in queue): '+url)
+		try :
+			launchvideo(url, False)
+		except :
+			logging.error('Error in launchvideo function.')
+			os.system("cat images/error.asc | wall")
 
 @app.route('/popcorn')
-def queue():
+def popcorn():
+	logging.info('Starting popcorntime function.')
+
         url = request.query['url']
+	logging.info('URL is :'+url)
+
 	ip = request.environ['REMOTE_ADDR']
+	logging.info('IP is :'+ip)
+
 	port = url.split(":")[2]
+	logging.info('Port is:'+port)
+
 	url = "http://"+ip+":"+port
+	logging.info('Final url:'+url)
 
-	print "downloading subtitle"
-
-	# Try to remove subtitle	
+	# Try to remove subtitle
 	os.system("rm subtitle.srt")
-	wget = "wget http://"+ip+":9999/subtitle.srt"
-	os.system(wget)
-
-	launchvideo(url, True)
+		
+	try :
+		os.system("wget http://"+ip+":9999/subtitle.srt")
+		logging.info('Success with Wget ! Starting with subtitles.')
+		try :
+			launchvideo(url, True)
+		except :
+			logging.error('Error in launchvideo function (with subtitles).')
+			os.system("cat images/error.asc | wall")
+	except :
+		logging.info('Error with Wget, starting without subtitles.')
+	
+		try :
+			launchvideo(url, False)
+		except :
+			logging.error('Error in launchvideo function (without subtitles).')
+			os.system("cat images/error.asc | wall")
+	
 
 @app.route('/video')
 def video():
 	control = request.query['control']
 	if control == "pause" :
+		logging.info('Command : pause')
 		os.system("echo -n p > /tmp/cmd")
 	elif control == "stop" :
+		logging.info('Command : stop')
 		os.system("echo -n q > /tmp/cmd")
 		os.system("cat images/stop.asc | wall")
 	elif control == "right" :
+		logging.info('Command : forward')
 		os.system("echo -n $'\x1b\x5b\x43' > /tmp/cmd")
 	elif control == "left" :
+		logging.info('Command : backward')
 		os.system("echo -n $'\x1b\x5b\x44' > /tmp/cmd")
 	elif control == "emptyqueue" :
+		logging.info('Command : empty queue file')
 		#Empty queue file
 		open('video.queue', 'w').close()
 
@@ -78,8 +117,10 @@ def sound():
 	vol = request.query['vol']
 	print vol + " volume"
 	if vol == "more" :
+		logging.info('Command : Sound ++')
 		os.system("echo -n + > /tmp/cmd")
 	elif vol == "less" :
+		logging.info('Command : Sound --')
 		os.system("echo -n - > /tmp/cmd")
 	
 run(app, reloader=False, host='0.0.0.0', debug=True, port=2020)
