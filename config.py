51e4852d101ae42be8a717cbca56ba885aacd555
vince <vincent.loewert@gmail.com>
2015-03-28 01:36:30 +0100

    better names for files of remote

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # Preferences
 
 # Set to True if you have a slow Internet connection, it will stream videos in 360p = False
- low_mode = True
+low_mode = True
 
 # Audio output : hdmi or local (using the jack) or both (jack & hdmi) (default)
- audio_output = "both"
+audio_output = "both"
