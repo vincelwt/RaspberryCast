cde74eacdc2e3ca2c90ff47b277e0ca91cce33fe
vince <vincent.loewert@gmail.com>
2015-03-28 01:22:52 +0100

    New restart mode

--- RaspberryCast.sh
+++ RaspberryCast.sh
@@ -24,6 +24,28 @@ elif [ $1 = "stop" ] ; then
 	echo "Done."
 	exit
 
+elif [ $1 = "restart" ] ; then
+	if [ `id -u` -ne 0 ]
+	then
+		echo "Please start this script with root privileges!"
+		echo "Try again with sudo."
+		exit 0
+	fi
+
+	echo "Killing RaspberryCast..."
+	killall omxplayer.bin >/dev/null 2>&1
+	killall python >/dev/null 2>&1
+	kill $(lsof -t -i :2020) >/dev/null 2>&1
+	rm *.pyc >/dev/null 2>&1
+	rm *.srt >/dev/null 2>&1
+	echo "Done."
+	echo "Starting server."
+	su - pi -c "./server.py &"
+	su - pi -c "./daemon_queue.py &"
+	echo "Done."
+	exit
+
+
 else
 	echo "Error, wrong argument. Try with 'stop' or 'start'."
 	exit
