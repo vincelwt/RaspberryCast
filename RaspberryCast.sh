bc9cc703630758d0c7997156ea8a880bb848f6a4
vince <vincent.loewert@gmail.com>
2015-03-28 11:07:11 +0100

    Sudo protection and fix restart

--- RaspberryCast.sh
+++ RaspberryCast.sh
@@ -2,6 +2,12 @@
 cd /home/pi/RaspberryCast/
 
 if [ $1 = "start" ]; then
+	if [ `id -u` -eq 0 ]
+	then
+		echo "Please start this script without root privileges!"
+		echo "Try again without sudo."
+		exit 0
+	fi
 	echo "Starting server."
 	./server.py &
 	./daemon_queue.py &
@@ -14,7 +20,6 @@ elif [ $1 = "stop" ] ; then
 		echo "Try again with sudo."
 		exit 0
 	fi
-
 	echo "Killing RaspberryCast..."
 	killall omxplayer.bin >/dev/null 2>&1
 	killall python >/dev/null 2>&1
@@ -23,30 +28,10 @@ elif [ $1 = "stop" ] ; then
 	rm *.srt >/dev/null 2>&1
 	echo "Done."
 	exit
-
 elif [ $1 = "restart" ] ; then
-	if [ `id -u` -ne 0 ]
-	then
-		echo "Please start this script with root privileges!"
-		echo "Try again with sudo."
-		exit 0
-	fi
-
-	echo "Killing RaspberryCast..."
-	killall omxplayer.bin >/dev/null 2>&1
-	killall python >/dev/null 2>&1
-	kill $(lsof -t -i :2020) >/dev/null 2>&1
-	rm *.pyc >/dev/null 2>&1
-	rm *.srt >/dev/null 2>&1
-	echo "Done."
-	echo "Starting server."
-	su - pi -c "/home/pi/RaspberryCast/server.py &"
-	su - pi -c "/home/pi/RaspberryCast/daemon_queue.py &"
-	echo "Done."
-	exit
-
-
+        ./RaspberryCast.sh stop
+        su - pi -c "./RaspberryCast.sh start"
 else
-	echo "Error, wrong argument. Try with 'stop' or 'start'."
+	echo "Error, wrong argument. Try with 'stop', 'start' or 'restart'."
 	exit
 fi
