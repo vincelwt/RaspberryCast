77622b7e792ddad2dd3203de91b3cb46cd7ef182
vince <vincent.loewert@gmail.com>
2015-03-29 22:16:44 +0200

    Functions merged

--- chrome/js/background.js
+++ chrome/js/background.js
@@ -1,61 +1,26 @@
-function stopNote1() {
-	chrome.notifications.clear('notif1', function(id) { console.log("Last error:", chrome.runtime.lastError); });
+function stopNote() {
+	chrome.notifications.clear('notif', function(id) { console.log("Last error:", chrome.runtime.lastError); });
 }
 
-function stopNote2() {
-	chrome.notifications.clear('notif2', function(id) { console.log("Last error:", chrome.runtime.lastError); });
-}
-
-function stopNote3(n) {
-	chrome.notifications.clear('notif3', function(id) { console.log("Last error:", chrome.runtime.lastError); });
-}
-
-function notif1() {
+function notif(msg) {
 	var opt = {
 		type: "basic",
 		title: "Raspberry Pi",
-		message: "Trying to get video stream URL. Please wait ~ 10-30 seconds.",
+		message: msg,
 		iconUrl: "48.png"
 	};
 
-	chrome.notifications.create('notif1', opt, function(id) { console.log("Last error:", chrome.runtime.lastError); });
+	chrome.notifications.create('notif', opt, function(id) { console.log("Last error:", chrome.runtime.lastError); });
 
 	setTimeout(stopNote1, 4000);		
 }
 
-function notif2() {
-	var opt = {
-		type: "basic",
-
-		title: "Raspberry Pi",
-		message: "Success ! Video should now be playing.",
-		iconUrl: "48.png"
-	};
-
-	chrome.notifications.create('notif2', opt, function(id) { console.log("Last error:", chrome.runtime.lastError); });
-
-	setTimeout(stopNote2, 4000);		
-}
-
-function notif3() {
-	var opt = {
-		type: "basic",
-		title: "Raspberry Pi",
-		message: "An error occured during the treatment of the link. Please make sure the link is compatible",
-		iconUrl: "48.png"
-	};
-
-	chrome.notifications.create('notif3', opt, function(id) { console.log("Last error:", chrome.runtime.lastError); });
-
-	setTimeout(stopNote3, 4000);		
-}
 
 function mkrequest(url, response) {
 	try {
 		var newURL = "http://"+localStorage.getItem('raspip')+":2020"+url;
 		if (response == 1) {
-			notif1();
-			//window.close();
+			notif("Trying to get video stream URL. Please wait ~ 10-30 seconds.");
 		}
 		var req = new XMLHttpRequest();
 		req.open('GET', newURL, true);
@@ -64,14 +29,14 @@ function mkrequest(url, response) {
 				if (req.status == 200) {
 					if (req.responseText == "1") {
 						if (response == 1) {
-							notif2();	
+							notif("Success ! Video should now be playing.");	
 						}
 					} else {
-						notif3();
+						notif("An error occured during the treatment of the link. Please make sure the link is compatible");
 					}
 				} else {
-					chrome.notifications.clear('notif1', function(id) { console.log("Last error:", chrome.runtime.lastError); });
-					alert("Error during connecting requesting from server !");
+					chrome.notifications.clear('notif', function(id) { console.log("Last error:", chrome.runtime.lastError); });
+					alert("Error during requesting from server ! Make sure the ip/port are corrects, and the server is running.");
 				}
 			}
 		};
