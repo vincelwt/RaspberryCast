d26ac599d19c5c9c43b4d3fc1b1f39297c231f37
Vincent Lwt <vincent.loewert@gmail.com>
2015-06-19 22:45:17 +0200

    Update config.py comments

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 # Preferences
 
-# Set to True if you have a slow Internet connection, it will stream videos in 360p = False
+# Set to True if you have a slow Internet connection, it will stream videos in 360p
 low_mode = True
 
 # Audio output : hdmi or local (using the jack) or both (jack & hdmi) (default)
 sound_output = "both"
 
-#Enable the new cool dialog info system (of course you want)
+#Enable the new cool dialog info system (default: True) disable for debug
 new_log = True
