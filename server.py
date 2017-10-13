a8d0777fe474ede7a7dcbc7f97c255419e882f25
Marcus Heine <mheine@kth.se>
2017-10-13 10:46:28 +0200

    Indentation issue

--- server.py
+++ server.py
@@ -72,7 +72,7 @@ def stream():
 			if ('localhost' in subtitles) or ('127.0.0.1' in subtitles):
 				ip = request.environ['REMOTE_ADDR']
                 logger.debug('Subtitle path contains localhost adress. Replacing with remote IP.')
-       			subtitles = subtitles.replace('localhost', ip).replace('127.0.0.1', ip)
+                subtitles = subtitles.replace('localhost', ip).replace('127.0.0.1', ip)
 
 			logger.debug('Subtitles link is '+subtitles)
 			urllib.urlretrieve(subtitles, "subtitle.srt")
